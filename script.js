109d24a4089b653def2b88b98a061611c4c00f34
RoddPH <170872082+RoddPH@users.noreply.github.com>
2026-02-24 22:27:29 +0800

    Added script.js
    Added a function that returns a random number and
    based on that number it will equal rock paper or
    scissors also added a console log for debugging

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    
+    if (random == 1) {
+        return "rock";
+    } else if (random == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
